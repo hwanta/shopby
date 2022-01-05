3356c0cbe7673ea8aaac931226cbef714a02d598
reomoon <hwanta7@gmail.com>
2022-01-05 14:29:23 +0900

    login

---
+++ Test Suites/login.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>68e286ab-83f4-401e-b4d1-e636db1a87c2</testSuiteGuid>
+   <testCaseLink>
+      <guid>03fc8c41-0204-40b6-85d7-adfb16e6a1d9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/login</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
