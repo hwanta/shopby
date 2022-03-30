9fe53a6ece1ad6ecc6a2eb1c9639a1a244b0f3b9
ByungWookLee <byungwook.lee@nhnsoft.com>
2022-03-30 13:38:54 +0900

    add directory

---
+++ testcase/testcase_option.py
@@ -0,0 +1,42 @@
+import sys, os # 폴더 다를 때 사용
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+from pages import login, option
+import time
+
+# 로그인
+login.login()
+
+# 설정 > 기본정책
+option.basicInformation()
+
+option.mallManagement()
+
+option.terms()
+
+option.externalService()
+
+# # 설정 > 관리정책
+option.operatorManagement()
+
+option.permissionGroup()
+
+# # 설정 > 보안정책
+option.operationalSecuritySettings()
+
+option.personalInformationAccess()
+
+option.securityManagementServer()
+
+# 설정 > 배송정책
+option.shippingCostManagement()
+
+# 설정 > 결제정책
+option.pgSettings()
+
+option.paycoPaymentSettings()
+
+option.naverPaymentSettings()
+
+time.sleep(5) # 5초 대기
+login.driver.quit()
