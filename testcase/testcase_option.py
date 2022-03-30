6b754560d10ae6abe9d0ec06633e49ca268b4102
ByungWookLee <byungwook.lee@nhnsoft.com>
2022-03-30 14:41:32 +0900

    change code comments

--- testcase/testcase_option.py
+++ testcase/testcase_option.py
@@ -1,5 +1,5 @@
-import sys, os # 폴더 다를 때 사용
-sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+import sys, os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__)))) # 폴더 다를 때 사용
 
 from pages import login, option
 import time
