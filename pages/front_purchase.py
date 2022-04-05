bf3264be5528731e4c70d10071fc48570e2a93fb
ByungWookLee <byungwook.lee@nhnsoft.com>
2022-04-05 16:45:14 +0900

    add front_purchase.py, tc_front_purchase.py

---
+++ pages/front_purchase.py
@@ -0,0 +1,44 @@
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait   # 로딩될 때까지 대기
+from selenium.webdriver.support import expected_conditions as EC # 로딩될 때까지 대기
+from pages import front_login
+
+# 상품 구매
+def productPurchase():
+
+    # 신규 카테고리 진입
+    WebDriverWait(front_login.driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT,
+                                                '신규 카테고리')))
+    front_login.driver.find_element(by=By.LINK_TEXT, value="신규 카테고리").click()
+
+    # 코듀로이 셔츠 무배 클릭
+    WebDriverWait(front_login.driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT,
+                                                                                '코듀로이 셔츠 무배')))
+    front_login.driver.find_element(by=By.LINK_TEXT, value="코듀로이 셔츠 무배").click()
+
+    time.sleep(0.5)
+    # 주문하기 버튼 클릭
+    WebDriverWait(front_login.driver, 10).until(EC.presence_of_element_located((By.ID,
+                                                                                'orderBtn')))
+    front_login.driver.find_element(by=By.ID, value="orderBtn").click()
+
+    # 무통장입금 라디오버튼 클릭
+    WebDriverWait(front_login.driver, 10).until(EC.presence_of_element_located((By.ID,
+                                                                                'paymethod-ACCOUNT')))
+
+    front_login.driver.execute_script("window.scrollTo(0, 1700)")
+
+    front_login.driver.find_element(by=By.CSS_SELECTOR, value="#payMethodList > div:nth-child(2) > label").click()
+
+    # 입금자명 입력
+    front_login.driver.find_element(by=By.NAME, value="remitter").send_keys('123')
+
+    # 개인정보 수집 동의/ 구매진행 동의
+    front_login.driver.find_element(by=By.CSS_SELECTOR, value="#termsChecks > div:nth-child(1) > label").click()
+    front_login.driver.find_element(by=By.CSS_SELECTOR, value="#termsChecks > div:nth-child(3) > label").click()
+
+    # 결제하기 버튼 클릭
+    front_login.driver.find_element(by=By.ID, value="orderBuy").click()
+
+
