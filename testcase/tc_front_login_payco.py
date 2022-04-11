c44e4d2824db4c0f4022f1f673af78b214bc735f
ByungWookLee <byungwook.lee@nhnsoft.com>
2022-04-11 16:17:37 +0900

    add payco login

---
+++ testcase/tc_front_login_payco.py
@@ -0,0 +1,10 @@
+import sys, os # 폴더 다를 때 사용
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+from pages import front_login_payco
+import time
+
+# 로그인
+front_login_payco.login()
+time.sleep(5) # 5초 대기
+front_login_payco.driver.quit()
