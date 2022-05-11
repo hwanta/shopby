7c8ded185c1c14cc54f64cd483e1c94390773c59
ByungWookLee <byungwook.lee@nhnsoft.com>
2022-05-11 16:29:38 +0900

    restore

--- tests/setting/info.py
+++ tests/setting/info.py
@@ -2,7 +2,7 @@ import sys, os # 폴더 다를 때 사용
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
 from pages import login
-from pages.setting.info import info
+from pages.setting import info
 import time
 
 
--- tests/setting/mainStatus.py
+++ tests/setting/mainStatus.py
@@ -2,7 +2,7 @@ import sys, os # 폴더 다를 때 사용
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
 from pages import login
-from pages.setting.mainStatus import mainStatus
+from pages.setting import mainStatus
 import time
 
 
